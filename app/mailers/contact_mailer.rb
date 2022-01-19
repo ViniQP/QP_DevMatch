820eef683f082a4077d11f9436a47eae56eb5f7b
Vinícius Moreno <sviniciusmoreno@gmail.com>
2022-01-19 03:58:33 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'viniqpoweryt@gmail.com'
+   default to: 'viniqpowerqp@gmail.com'
    
    def contact_email(name, email, body)
       @name = name
