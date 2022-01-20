8a670348efdcd47936a2bace113051762002a86d
Vinícius Moreno <sviniciusmoreno@gmail.com>
2022-01-20 05:02:41 +0000

    Updated nav link to helpers

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
     get 'contact-us', to: 'contacts#new', as: 'new_contact'
+    get 'sign-in', to: 'devise/sessions#new', as: 'new_user_session'
 end
 
