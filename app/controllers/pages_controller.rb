1aea2cb580fe44bed10b7d717e00b50142146962
Vinícius Moreno <sviniciusmoreno@gmail.com>
2022-01-27 08:15:37 +0000

    Added a create profile button in the navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,6 @@ class PagesController < ApplicationController
         @basic_plan = Plan.find(1)
         @pro_plan  = Plan.find(2)
         
-        @user = User.find(current_user)
- 
     end 
     
     def about
